0875fbe90f6cfeb97f0c15ee862e34811fc33c76
jellyfishgh <1129088994@qq.com>
2016-10-29 00:39:49 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular2-quickstart-ts
+angular2 quickstart powered by typescript
---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template:  `
+    <h1>My First Angular2 App</h1>
+    <h2>Hello World</h2>
+    `
+})
+export class AppComponent {}
